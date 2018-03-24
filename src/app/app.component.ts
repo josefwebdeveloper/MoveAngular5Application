f1c5abfa7c7de62ff970efd0643cb29adebd9f02
Josef Shainskiy <josef2011@gmail.com>
2018-03-25 01:03:29 +0300

    prepare for heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Move_Responsive';
+  currentPosition = {currentX: 45, currentY: 45};
+
+
+  left() {
+    if (this.currentPosition.currentX > 0.4) {
+      this.currentPosition.currentX = this.currentPosition.currentX - 0.4;
+    } else {
+      this.currentPosition.currentX = 0;
+    }
+    console.log(this.currentPosition);
+    // this.currentPosition = {currentX: x, currentY: y};
+  }
+
+  right() {
+    if (this.currentPosition.currentX < 89.6) {
+      this.currentPosition.currentX = this.currentPosition.currentX + 0.4;
+      console.log(this.currentPosition);
+    } else {
+      this.currentPosition.currentX = 90;
+    }
+    // this.currentPosition = {currentX: x, currentY: y};
+  }
+
+  up() {
+    if (this.currentPosition.currentY > 0.4) {
+      this.currentPosition.currentY = this.currentPosition.currentY - 0.4;
+    } else {
+      this.currentPosition.currentY = 0;
+    }
+    console.log(this.currentPosition);
+    // this.currentPosition = {currentX: x, currentY: y};
+  }
+  down() {
+    if (this.currentPosition.currentY < 89.6) {
+      this.currentPosition.currentY = this.currentPosition.currentY + 0.4;
+    } else {
+      this.currentPosition.currentY = 90;
+    }
+    console.log(this.currentPosition);
+    // this.currentPosition = {currentX: x, currentY: y};
+  }
+  reset(){
+    this.currentPosition = {currentX: 45, currentY: 45};
+  }
 }
